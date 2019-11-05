8809a7a849acac4f443a12d99d668220ecaf11e5
Melissa Phillips <coyphillips@gmail.com>
2019-11-04 21:36:39 -0500

    updated html,css,js to fix errors

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
 'use strict';
-const apiKey ='vkjEfxb65gj444Dn2Dl1pV7R3e1b9RAqma2ZYE34';
-const url = 'developer.nps.gov/api/v1/parks ';
 
-function formatQueryParams(params){
-    const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
+  return queryItems.join('&');    
 }
-function getParkList(pickState, maxResultst=10){
 
+function parkList(searchUrl, stateAbrv, maxResults, apiKey) {
+    
     const params = {
-        api_key : apiKey,
-        stateCode: pickState,
-        limit: maxResultst
+        stateCode: stateAbrv,
+        limit: maxResults
     }
-    const queryString = generateQueryString(params);
-    const url = searchUrl + '?' + queryString;
+    
+    const queryString = formatQueryParams(params);
+    const url = searchUrl + '?' + queryString + '&api_key=' + apiKey;
 
     fetch(url)
     .then(response => {
-        if(response.ok){
+        if (response.ok) {
             return response.json();
-        } else {
-            throw new Error (response.statusText);
         }
+        throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
-    .catch(error => {
-        $('.error-message').text(`Something went wrong: ${error.message}`);
-    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+        $('.js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
-function displayResults(responseJson){
-    $('#results').empty();
-    for(let i=0; i < responseJson.data.length; i++){
-        $('#results').append(`
-        <li><h2>${responseJson.data[i].name}<h2>
-        <p>Located In: ${responseJson.data[i].description}</p>
-        <a href="${responseJson.data[i].url} target="_blank">View Website</a>
-        </li>`
-    )}
+
+function displayResults(responseJson, maxResults) {
+    $('#js-error-message').empty();
+    $('#results-list').empty();
+    for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        $('#results-list').append(`
+          <li><h2>${responseJson.data[i].fullName}</h2>
+            <p>${responseJson.data[i].description}</p>       
+            <p><a href="${responseJson.data[i].url}" target="_blank">visit park website</a></p>    
+          </li>`
+         );
+    }
     $('#results').removeClass('hidden');
-};
-function watchForm(){
-    $('form').submit(event => {
-        event.preventDefault();
-        const searchState = $('#js-search-term').val();
-        const pickState = searchState.replace(/\s+/g, '&statecode=');
-        const maxResults = $('#js-max-results').val();
-        getParkList(pickState, maxResults);
-    })
 }
+
+function watchForm() {
+  $('#js-form').on('submit', function(event) { 
+    event.preventDefault();
+    let stateAbrv = $('#js-search-term').val().split(",");
+    let maxResults = $('#js-max-results').val();
+    let apiKey = 'vkjEfxb65gj444Dn2Dl1pV7R3e1b9RAqma2ZYE34';
+    let searchURL = 'https://developer.nps.gov/api/v1/parks';
+    parkList(searchURL, stateAbrv, maxResults, apiKey);
+  })  
+}
+
 $(watchForm);
